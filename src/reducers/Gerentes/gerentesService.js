990e41005a3d329a12f91e0db876ab4659b13801
cmelgargiama <cmelgar@giama.com.ar>
2022-08-18 09:59:38 -0300

    Gerentes table Insert with bugs

--- src/reducers/Gerentes/gerentesService.js
+++ src/reducers/Gerentes/gerentesService.js
@@ -1,17 +1,25 @@
 import axios from 'axios'
-
 const API_URL = 'http://localhost:3001/'
 
 const getGerentes = async () => {
-
     const response = await axios.get(API_URL + 'gerentes')
     return response.data[0] 
   }
+const postGerentes = async (form) => {
+    const response = await axios.post(API_URL + 'gerentes', form )
+    return response.data 
+  }
+const updateGerentes = async () => {
+  const response = await axios.put(API_URL + 'gerentes')
+  return response.data[0] 
+}  
 
-
+const deleteGerentes = async (json) => {
+  const response = await axios.delete(API_URL + 'gerentes', json)
+  return response.data }
 
 const gerentesService = {
-    getGerentes
+    getGerentes, postGerentes, updateGerentes, deleteGerentes
   }
 
 
