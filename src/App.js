c2b526fe6097edd3a07ce345eb345d82a04817c7
franco <2>
2022-08-19 12:15:37 -0300

    reset password new users routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import { Login } from './components/Login/Login';
 import Home from './components/Home/Home';
+import PrivateRoute from './PrivateRoute';
 import SideBar from './components/SideBar/SideBar';
 import { ResetPassword } from './components/ForgotPassword/ResetPassword';
 import { RecoveryPass } from './components/ForgotPassword/RecoveryPass';
@@ -13,24 +14,26 @@ function App() {
   const {user} = useSelector(
     (state) => state.login)
   return (
-    user ?
+    user && (user?.newUser === 0 || user?.newUser === null || !user?.newUser) ?
     <div className="App">
         <SideBar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/gerentes' element={<GerentesTable/>}/>
           <Route path='/usuarios' element={<UsuariosTable/>}/>
-          <Route path='/altaUsuarios' element={<AltaUsuariosForm/>}/>
+          <Route path='/altaUsuarios' element={<PrivateRoute rol={'1.2.2'}/>}>
+              <Route path='/altaUsuarios' element={<AltaUsuariosForm/>}/>
+          </Route>
           <Route path='/modifUsuarios/:id' element={<AltaUsuariosForm/>}/>
         </Routes> 
         
     </div> :
+
     <Routes>
     <Route path='/' element={<Login/>}/>
     <Route path='/recovery' element={<RecoveryPass/>}/>
     <Route path='/reset-password/:id/:token' element={<ResetPassword/>}/>
-    </Routes>
-    
+    </Routes> 
    
   );
 }
