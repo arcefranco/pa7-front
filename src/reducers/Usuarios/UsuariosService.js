9965bf50998a76f64f4c15012ed12825cb43aeac
franco <2>
2022-08-31 14:27:58 -0300

    fix localhost

--- src/reducers/Usuarios/UsuariosService.js
+++ src/reducers/Usuarios/UsuariosService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import getHeaderToken from '../../helpers/getHeaderToken';
-const API_URL = 'http://ec2-52-33-204-119.us-west-2.compute.amazonaws.com:3001'
+const API_URL = 'http://localhost:3001/'
 const headers = getHeaderToken();
 
 const getUsuarioById = async(id) => {
