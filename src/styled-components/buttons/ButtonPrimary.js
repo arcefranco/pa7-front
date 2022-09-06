3445a0fa35feb1098093d4ed8188a8d38e42a289
franco <2>
2022-09-06 09:54:43 -0300

    giveMaster in rolesForm

--- src/styled-components/buttons/ButtonPrimary.js
+++ src/styled-components/buttons/ButtonPrimary.js
@@ -5,14 +5,18 @@ export default styled.button`
     padding: 0.5rem;
     border: none;
     background-color: #3c8dbc;
-    border-radius: 1.2em;
+    border-radius: 0.4em;
     box-shadow: none;
     color: white;
-    font-size: 1.3em;
+    font-size: 0.7em;
    
    
    
 &:hover{
        background-color: #1971a4
    }
+
+&:disabled{
+    background-color: gray;
+}
 `
