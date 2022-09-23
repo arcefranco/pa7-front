9920cfae2ef508baa55aa7f9758b1e0fdd52587e
franco <2>
2022-09-23 16:15:50 -0300

    fix modelos

--- src/components/ModelosTable/ModelosFormulario.jsx
+++ src/components/ModelosTable/ModelosFormulario.jsx
@@ -133,32 +133,38 @@ const ModelosFormulario = () =>{
     }, [modeloById]) 
 
     
-  
-    const inputArray =  [{ Codigo: id? id : null,
-      Nombre: modeloById[0]?.Nombre ? modeloById[0]?.Nombre : null, 
-      Activo: modeloById[0]?.Activo,
-      NacionalImportado: modeloById[0]?.NacionalImportado,
-      HechoPor: user.username,
-      CodigoMarca: user.codigoMarca,
-    }]
+  useEffect(() => {
+    if(modeloById){
+      
+      setInput({ Codigo: id? id : null,
+        Nombre: modeloById[0]?.Nombre ? modeloById[0]?.Nombre : null, 
+        Activo: modeloById[0]?.Activo ? modeloById[0]?.Activo : null,
+        NacionalImportado: modeloById[0]?.NacionalImportado ? modeloById[0]?.NacionalImportado : null,
+        HechoPor: user.username,
+        CodigoMarca: user.codigoMarca,
+      })
+    }
+  }, [modeloById])
         
       
     useEffect(() => {
-    setInput(...inputArray)
 
-    setCuotas(
-      tipoPlan.map(plan => {
-        return {
-          ["CuotaTerminal_" + plan.ID]: typeof modeloById[0]?.["CuotaTerminal_" + plan.ID] === 'string' ? parseInt(modeloById[0]?.["CuotaTerminal_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-          ["CuotaACobrar_" + plan.ID]: typeof modeloById[0]?.["CuotaACobrar_" + plan.ID] === 'string' ? parseInt(modeloById[0]?.["CuotaACobrar_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-          ["CuotaACobrarA_" + plan.ID]: typeof modeloById[0]?.["CuotaACobrarA_" + plan.ID] === 'string' ? parseInt(modeloById[0]?.["CuotaACobrarA_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-          ["Cuota1_" + plan.ID]: typeof modeloById[0]?.["Cuota1_" + plan.ID] === 'string' ? parseInt(modeloById[0]?.["Cuota1_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-          ["Cuota2_" + plan.ID]: typeof modeloById[0]?.["Cuota2_" + plan.ID] === 'string' ? parseInt(modeloById[0]?.["Cuota2_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-          TipoPlan: plan.ID - 1,
-          Descripcion: plan.Descripcion
-          
-          }
-      }))
+      if(modeloById){
+        
+        setCuotas(
+          tipoPlan.map(plan => {
+            return {
+              ["CuotaTerminal_" + plan.ID]: typeof modeloById[0]?.["CuotaTerminal_" + plan.ID] === 'string' ? parseInt(modeloById[0]?.["CuotaTerminal_" + plan.ID]).toFixed(2) : /* parseFloat(0.00).toFixed(2) */"",
+              ["CuotaACobrar_" + plan.ID]: typeof modeloById[0]?.["CuotaACobrar_" + plan.ID] === 'string' ? parseInt(modeloById[0]?.["CuotaACobrar_" + plan.ID]).toFixed(2) : /* parseFloat(0.00).toFixed(2) */"",
+              ["CuotaACobrarA_" + plan.ID]: typeof modeloById[0]?.["CuotaACobrarA_" + plan.ID] === 'string' ? parseInt(modeloById[0]?.["CuotaACobrarA_" + plan.ID]).toFixed(2) : /* parseFloat(0.00).toFixed(2) */"",
+              ["Cuota1_" + plan.ID]: typeof modeloById[0]?.["Cuota1_" + plan.ID] === 'string' ? parseInt(modeloById[0]?.["Cuota1_" + plan.ID]).toFixed(2) : /* parseFloat(0.00).toFixed(2) */"",
+              ["Cuota2_" + plan.ID]: typeof modeloById[0]?.["Cuota2_" + plan.ID] === 'string' ? parseInt(modeloById[0]?.["Cuota2_" + plan.ID]).toFixed(2) : /* parseFloat(0.00).toFixed(2) */"",
+              TipoPlan: plan.ID - 1,
+              Descripcion: plan.Descripcion
+              
+              }
+          }))
+      }
    
   }, [modeloById, tipoPlan]);
 
@@ -170,13 +176,12 @@ const HandleChange =  (e) =>{
   
     
     const {name , value} = e.target
-    console.log(value, name)
+
     const newForm = {...input,
       [name]:value,
       }
     
-    setInput(newForm )
-    console.log(newForm)
+    setInput(newForm)
     const errors = validateform(newForm);
     setError(errors);
   }
@@ -187,10 +192,8 @@ const HandleChange =  (e) =>{
     let changedCuota = nuevasCuotas[parseInt(e.target.name.slice(-1)) - 1]
     changedCuota = {...changedCuota, [name]: value}
 
-    // console.log(changedCuota)
     nuevasCuotas[parseInt(e.target.name.slice(-1)) - 1] = changedCuota
     setCuotas(nuevasCuotas)
-    // console.log(name, value)
 
   }
   const handleCheckChange = (e) => {
@@ -209,11 +212,11 @@ const formInput = [input, ...cuotas]
 
 tipoPlan.map(plan=>{
   updateArray.push( {
-    CuotaTerminal: typeof formInput[plan.ID ]?.["CuotaTerminal_" + plan.ID] === 'string' ? parseInt(formInput[plan.ID ]?.["CuotaTerminal_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-    CuotaACobrar: typeof formInput[plan.ID ]?.["CuotaACobrar_" + plan.ID] === 'string' ? parseInt(formInput[plan.ID ]?.["CuotaACobrar_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-    CuotaACobrarA: typeof formInput[plan.ID ]?.["CuotaACobrarA_" + plan.ID] === 'string' ? parseInt(formInput[plan.ID ]?.["CuotaACobrarA_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-    Cuota1: typeof formInput[plan.ID ]?.["Cuota1_" + plan.ID] === 'string' ? parseInt(formInput[plan.ID ]?.["Cuota1_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-    Cuota2: typeof formInput[plan.ID ]?.["Cuota2_" + plan.ID] === 'string' ? parseInt(formInput[plan.ID ]?.["Cuota2_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
+    CuotaTerminal: typeof formInput[plan.ID ]?.["CuotaTerminal_" + plan.ID] === 'string' && formInput[plan.ID ]?.["CuotaTerminal_" + plan.ID].length ? parseInt(formInput[plan.ID ]?.["CuotaTerminal_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
+    CuotaACobrar: typeof formInput[plan.ID ]?.["CuotaACobrar_" + plan.ID] === 'string' && formInput[plan.ID ]?.["CuotaACobrar_" + plan.ID].length ? parseInt(formInput[plan.ID ]?.["CuotaACobrar_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
+    CuotaACobrarA: typeof formInput[plan.ID ]?.["CuotaACobrarA_" + plan.ID] === 'string' && formInput[plan.ID ]?.["CuotaACobrarA_" + plan.ID].length ? parseInt(formInput[plan.ID ]?.["CuotaACobrarA_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
+    Cuota1: typeof formInput[plan.ID ]?.["Cuota1_" + plan.ID] === 'string' && formInput[plan.ID ]?.["Cuota1_" + plan.ID].length ? parseInt(formInput[plan.ID ]?.["Cuota1_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
+    Cuota2: typeof formInput[plan.ID ]?.["Cuota2_" + plan.ID] === 'string' && formInput[plan.ID ]?.["Cuota2_" + plan.ID].length ? parseInt(formInput[plan.ID ]?.["Cuota2_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
     TipoPlan: plan.ID - 1,
     Descripcion: plan.Descripcion,
     CodigoMarca: user.codigoMarca,
@@ -235,12 +238,12 @@ const HandleSubmitUpdate =async (event) =>{
   const formInput = [input, ...cuotas] 
 
 tipoPlan.map(plan=>{
-  updateArray.push( {
-    CuotaTerminal: typeof formInput[plan.ID ]?.["CuotaTerminal_" + plan.ID] === 'string' ? parseInt(formInput[plan.ID ]?.["CuotaTerminal_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-    CuotaACobrar: typeof formInput[plan.ID ]?.["CuotaACobrar_" + plan.ID] === 'string' ? parseInt(formInput[plan.ID ]?.["CuotaACobrar_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-    CuotaACobrarA: typeof formInput[plan.ID ]?.["CuotaACobrarA_" + plan.ID] === 'string' ? parseInt(formInput[plan.ID ]?.["CuotaACobrarA_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-    Cuota1: typeof formInput[plan.ID ]?.["Cuota1_" + plan.ID] === 'string' ? parseInt(formInput[plan.ID ]?.["Cuota1_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
-    Cuota2: typeof formInput[plan.ID ]?.["Cuota2_" + plan.ID] === 'string' ? parseInt(formInput[plan.ID ]?.["Cuota2_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
+  updateArray.push({
+    CuotaTerminal: typeof formInput[plan.ID ]?.["CuotaTerminal_" + plan.ID] === 'string' && formInput[plan.ID ]?.["CuotaTerminal_" + plan.ID].length ? parseInt(formInput[plan.ID ]?.["CuotaTerminal_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
+    CuotaACobrar: typeof formInput[plan.ID ]?.["CuotaACobrar_" + plan.ID] === 'string' && formInput[plan.ID ]?.["CuotaACobrar_" + plan.ID].length ? parseInt(formInput[plan.ID ]?.["CuotaACobrar_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
+    CuotaACobrarA: typeof formInput[plan.ID ]?.["CuotaACobrarA_" + plan.ID] === 'string' && formInput[plan.ID ]?.["CuotaACobrarA_" + plan.ID].length ? parseInt(formInput[plan.ID ]?.["CuotaACobrarA_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
+    Cuota1: typeof formInput[plan.ID ]?.["Cuota1_" + plan.ID] === 'string' && formInput[plan.ID ]?.["Cuota1_" + plan.ID].length ? parseInt(formInput[plan.ID ]?.["Cuota1_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
+    Cuota2: typeof formInput[plan.ID ]?.["Cuota2_" + plan.ID] === 'string' && formInput[plan.ID ]?.["Cuota2_" + plan.ID].length ? parseInt(formInput[plan.ID ]?.["Cuota2_" + plan.ID]).toFixed(2) : parseFloat(0.00).toFixed(2),
     TipoPlan: plan.ID - 1,
     Descripcion: plan.Descripcion,
     CodigoMarca: user.codigoMarca,
