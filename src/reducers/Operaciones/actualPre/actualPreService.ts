ff45dd8ad3d0e302ab88a15d4517aaebb9d75b9e
franco <2>
2023-02-27 09:59:10 -0300

    altaPre fix

--- src/reducers/Operaciones/actualPre/actualPreService.ts
+++ src/reducers/Operaciones/actualPre/actualPreService.ts
@@ -1,178 +1,133 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { errorsHandling } from "../../errorsHandling";
 import getHeaderToken from "../../../helpers/getHeaderTokenAndDB";
+import { getFunction, postFunction } from "../../Axios/axiosFunctions";
+import { ServiceErrorHandler } from "../../../helpers/ServiceErrorHandler";
 
 const getPreOperaciones = async (data) => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .post(
+  try {
+    const headers = getHeaderToken();
+    const response = await axios.post(
       process.env.REACT_APP_HOST + "Operaciones/ActualPre/solicitudes",
       data,
       headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+    );
+
+    if (Array.isArray(response.data)) {
+      return response.data;
+    } else {
+      throw response.data;
+    }
+  } catch (error: any | AxiosError) {
+    return ServiceErrorHandler(error, "Operaciones/AltaPre/verify");
+  }
 };
 
 const getDatosPreSol = async (data) => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .post(
+  try {
+    const headers = getHeaderToken();
+    const response = await axios.post(
       process.env.REACT_APP_HOST + "Operaciones/ActualPre/datosOp",
       data,
       headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+    );
+
+    if (response.data.hasOwnProperty("Numero")) {
+      return response.data;
+    } else {
+      throw response.data;
+    }
+  } catch (error) {
+    return ServiceErrorHandler(error, "Operaciones/AltaPre/fechaCont");
+  }
 };
 
 const getModelos = async () => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .get(process.env.REACT_APP_HOST + "Operaciones/ActualPre/modelos", headers)
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return getFunction("Operaciones/ActualPre/modelos");
 };
 
 const getOficialesMora = async () => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .get(
-      process.env.REACT_APP_HOST + "Operaciones/ActualPre/oficialesmora",
-      headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return getFunction("Operaciones/ActualPre/oficialesmora");
 };
 
 const getOficialesPC = async () => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .get(
-      process.env.REACT_APP_HOST + "Operaciones/ActualPre/oficialespc",
-      headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return getFunction("Operaciones/ActualPre/oficialespc");
 };
 
 const getOficialesScoring = async () => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .get(
-      process.env.REACT_APP_HOST + "Operaciones/ActualPre/oficialesScoring",
-      headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return getFunction("Operaciones/ActualPre/oficialesScoring");
 };
 
 const getOrigenSuscripcion = async () => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .get(process.env.REACT_APP_HOST + "Operaciones/ActualPre/origen", headers)
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return getFunction("Operaciones/ActualPre/origen");
 };
 
 const getPuntosVenta = async () => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .get(
-      process.env.REACT_APP_HOST + "Operaciones/ActualPre/puntosventa",
-      headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return getFunction("Operaciones/ActualPre/puntosventa");
 };
 
 const getParametros = async () => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .get(
+  try {
+    const headers = getHeaderToken();
+    const response = await axios.get(
       process.env.REACT_APP_HOST + "Operaciones/ActualPre/parametros",
       headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+    );
+
+    if (
+      response.data.hasOwnProperty("soli") ||
+      response.data.hasOwnProperty("sanu")
+    ) {
+      return response.data;
+    } else {
+      throw response.data;
+    }
+  } catch (error) {
+    return ServiceErrorHandler(error, "Operaciones/AltaPre/fechaCont");
+  }
 };
 
 const getFormasPago = async () => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .get(
-      process.env.REACT_APP_HOST + "Operaciones/ActualPre/formasPago",
-      headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return getFunction("Operaciones/ActualPre/formasPago");
 };
 
 const getTarjetas = async () => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .get(process.env.REACT_APP_HOST + "Operaciones/ActualPre/tarjetas", headers)
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return getFunction("Operaciones/ActualPre/tarjetas");
 };
 
 const getIntereses = async () => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .get(
-      process.env.REACT_APP_HOST + "Operaciones/ActualPre/intereses",
-      headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return getFunction("Operaciones/ActualPre/intereses");
 };
 
 const pagoSenia = async (data) => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .post(
-      process.env.REACT_APP_HOST + "Operaciones/ActualPre/pagoSenia",
-      data,
-      headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return postFunction("Operaciones/ActualPre/pagoSenia", data);
 };
 
 const deletePago = async (data) => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .post(
-      process.env.REACT_APP_HOST + "Operaciones/ActualPre/deletePago",
-      data,
-      headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return postFunction("Operaciones/ActualPre/deletePago", data);
 };
 
 const updatePago = async (data) => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .post(
-      process.env.REACT_APP_HOST + "Operaciones/ActualPre/updatePago",
-      data,
-      headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+  return postFunction("Operaciones/ActualPre/updatePago", data);
 };
 
 const getSenias = async (data) => {
-  const headers = getHeaderToken();
-  const response = await axios
-    .post(
+  try {
+    const headers = getHeaderToken();
+    const response = await axios.post(
       process.env.REACT_APP_HOST + "Operaciones/ActualPre/datosOpSenias",
       data,
       headers
-    )
-    .catch((error) => errorsHandling(error));
-  return response.data;
+    );
+
+    if (Array.isArray(response.data)) {
+      return response.data;
+    } else {
+      throw response.data;
+    }
+  } catch (error: any | AxiosError) {
+    return ServiceErrorHandler(error, "Operaciones/AltaPre/verify");
+  }
 };
 
 const actualPreService = {
